f27468d943f8fc4dd7541a36495d649192dc2afc
zpc <zpcautomation@outlook.com>
2021-09-08 09:47:55 +0100

    first commit

---
+++ Basic/Contollers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basic.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var grandmaclaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "Bob@fmail.com"),
+                new Claim("Grandma.Says", "Very nice boi.")
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim("DrivingLicense", "A+"),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaclaims, "Grandma Identity");
+            var drivingLisenceIdentity = new ClaimsIdentity(grandmaclaims, "Government");
+
+            var userPricipal = new ClaimsPrincipal(new[] { grandmaIdentity ,drivingLisenceIdentity});
+
+            HttpContext.SignInAsync(userPricipal);
+            return RedirectToAction("Index");
+        }
+    }
+}
